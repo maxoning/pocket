b1476fc8fd86c28efcc80a7e72929540c3e7b6d3
ilotterytea <ilotterytea@proton.me>
2022-12-12 22:03:41 +0600

    Utilities that have to load anything.

---
+++ core/src/kz/ilotterytea/maxon/utils/LoadUtils.kt
@@ -0,0 +1,28 @@
+package kz.ilotterytea.maxon.utils
+
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.assets.loaders.SkinLoader
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+
+/**
+ * Utilities that have to load anything.
+ * @since a_1.0
+ */
+class LoadUtils {
+    companion object {
+        /**
+         * Load assets to the asset manager.
+         * @since a_1.0
+         */
+        fun loadAssets(m: AssetManager) {
+            // Texture atlases:
+            m.load("BrandSpritesheet.atlas", TextureAtlas::class.java)
+            m.load("ContribSpritesheet.atlas", TextureAtlas::class.java)
+            m.load("MainSpritesheet.atlas", TextureAtlas::class.java)
+
+            // Skins:
+            m.load("MainSpriteSheet.skin", Skin::class.java, SkinLoader.SkinParameter("MainSpritesheet.atlas"))
+        }
+    }
+}
