4ca0405a71bdd3020dc2f3208f9a6947fb171931
ilotterytea <ilotterytea@proton.me>
2022-12-12 21:29:25 +0600

    Initial commit

---
+++ core/src/kz/ilotterytea/maxon/MaxonGame.kt
@@ -0,0 +1,32 @@
+package kz.ilotterytea.maxon
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
+
+/**
+ * Main game.
+ * @since a_1.0
+ */
+class MaxonGame : ApplicationAdapter() {
+    lateinit var batch: SpriteBatch
+    lateinit var img: Texture
+
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render() {
+        ScreenUtils.clear(1f, 0f, 0f, 1f)
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
