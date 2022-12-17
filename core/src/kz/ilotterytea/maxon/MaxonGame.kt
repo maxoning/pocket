8922868513cf5d4525452ec5f23ce22e1120b91c
ilotterytea <ilotterytea@proton.me>
2022-12-18 00:15:15 +0600

    Load en_us localization.

--- core/src/kz/ilotterytea/maxon/MaxonGame.kt
+++ core/src/kz/ilotterytea/maxon/MaxonGame.kt
@@ -1,9 +1,11 @@
 package kz.ilotterytea.maxon
 
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import kz.ilotterytea.maxon.screens.SplashScreen
+import kz.ilotterytea.maxon.utils.I18N
 
 /**
  * Main game.
@@ -12,10 +14,12 @@ import kz.ilotterytea.maxon.screens.SplashScreen
 class MaxonGame : Game() {
     lateinit var batch: SpriteBatch
     lateinit var assets: AssetManager
+    lateinit var i18n: I18N
 
     override fun create() {
         batch = SpriteBatch()
         assets = AssetManager()
+        i18n = I18N(Gdx.files.internal("langs"), "en_us")
 
         this.screen = SplashScreen(this)
     }
