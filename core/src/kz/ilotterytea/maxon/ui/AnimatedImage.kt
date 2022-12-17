fd3efe0b7e7f5e0bf9bda2053d1112c2f796bfb4
ilotterytea <ilotterytea@proton.me>
2022-12-17 20:47:03 +0600

    Animated image widget.

---
+++ core/src/kz/ilotterytea/maxon/ui/AnimatedImage.kt
@@ -0,0 +1,31 @@
+package kz.ilotterytea.maxon.ui
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+
+/** Make the image animated! */
+class AnimatedImage(val textures: List<TextureRegion>) : Image() {
+    private var index = 0
+    var stopAnimation = false
+
+    init {
+        super.setDrawable(TextureRegionDrawable(textures[0]))
+    }
+
+    override fun act(delta: Float) {
+        super.act(delta)
+
+        if (!stopAnimation) {
+            next()
+        }
+    }
+
+    /** Change image's drawable to the next frame. */
+    fun next() {
+        index++
+
+        if (index > textures.size - 1) index = 0
+        super.setDrawable(TextureRegionDrawable(textures[index]))
+    }
+}
