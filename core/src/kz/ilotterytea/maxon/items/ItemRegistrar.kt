d2d62dc6c558d5ef4a82cae7b7e42eeb8a72381d
ilotterytea <ilotterytea@proton.me>
2022-12-20 22:48:58 +0600

    Register by num id

--- core/src/kz/ilotterytea/maxon/items/ItemRegistrar.kt
+++ core/src/kz/ilotterytea/maxon/items/ItemRegistrar.kt
@@ -7,17 +7,17 @@ import com.badlogic.gdx.Gdx
  * @since a_1.0
  */
 class ItemRegistrar<T : Registerable> {
-    val registeredItems = mutableMapOf<String, T>()
+    val registeredItems = mutableMapOf<Int, T>()
 
     /** Register an item.
      * @since a_1.0
      */
     fun register(item: T) {
-        if (registeredItems.containsKey(item.name_id)) {
+        if (registeredItems.containsKey(item.num_id)) {
             Gdx.app.debug("ItemRegistrar", "Item ID ${item.name_id} already registered! Abort.")
             return
         }
-        registeredItems[item.name_id] = item
+        registeredItems[item.num_id] = item
         Gdx.app.debug("ItemRegistrar", "Item ID ${item.name_id} successfully registered!")
     }
 }
